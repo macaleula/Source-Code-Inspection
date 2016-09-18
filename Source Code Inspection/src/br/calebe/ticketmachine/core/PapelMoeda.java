32a52d70dbde2d6dafca2636f7b15e80ce418140
macaleula <matheus.freire91@outlook.com.br>
2016-09-18 15:29:22 -0300

    Update PapelMoeda.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -6,19 +6,28 @@ package br.calebe.ticketmachine.core;
  */
 public class PapelMoeda {
 
-    protected int valor;
-    protected int quantidade;
+    private int valor;
+    private int quantidade;
 
     public PapelMoeda(int valor, int quantidade) {
         this.valor = valor;
         this.quantidade = quantidade;
-    }
+    }  
 
     public int getValor() {
         return valor;
     }
 
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
     public int getQuantidade() {
         return quantidade;
     }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    } 
 }
+
